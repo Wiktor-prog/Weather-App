84fdadf41f7e8bf1375f3d7aaa4365c28e12d6b0
Wiktor Ziejewski <wiktor.ziejewski>
2022-07-31 15:34:00 +0200

    add localstorage

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,14 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from "react-router-dom"; 
 
-function Details() {
+export default function Details() {
 
-const [city, setDetails] = useState("")
-const [detailsInfo, setDetailsInfo] = useState({})
+const [city, setDetails] = useState(["Warszawa"])
+const [detailsInfo, setDetailsInfo] = useState([])
+const APIkey = "5f120783051b409e82f14337220807"
+
 let navigate = useNavigate();
 
 const DetailsApi = () => {
-  fetch(`http://api.weatherapi.com/v1/forecast.json?key=5f120783051b409e82f14337220807&q=${city}&days=1&aqi=no&alerts=no`)
+  fetch([`http://api.weatherapi.com/v1/forecast.json?key=${APIkey}&q=${city}&days=1&aqi=no&alerts=no`])
   .then(response => response.json())
   .then((data) => setDetailsInfo({
     current: data.current.feelslike_c,
@@ -20,50 +22,43 @@ const DetailsApi = () => {
     condition: data.current.condition.text,
     img: data.current.condition.icon
   }))
-  
 };
 
 return (
-<div className="Details">
-  <FontAwesomeIcon 
-  className="icon" 
-  icon={ faArrowLeftLong }
-  onClick={() => {
-  navigate ("/");
-}}
-  />
+  <div className="Details">
+    <FontAwesomeIcon 
+    className="icon" 
+    icon={ faArrowLeftLong }
+    onClick={() => {
+    navigate ("/");
+  }}/>
 
   <h2>Weather Details</h2>
+      <button 
+      className="details-btn"
+      onChange={(event) => setDetails(event.target.value)} 
+      onClick={DetailsApi}>more details
+      </button>
 
-    <button 
-    className="details-btn"
-    onChange={(event) => setDetails(event.target.value)} 
-    onClick={DetailsApi}>more details
-    </button>
+      <input id="details-input"  
+        type ="text" 
+        placeholder="Search weather details in you city"
+        value={city}
+        onChange={(event) => setDetails(event.target.value)}
+      />
 
-    <input id="details-input"  
-      type ="text" 
-      placeholder="Search weather details in you city"
-      value={city}
-      onChange={(event) => setDetails(event.target.value)}
-    />
-
-    <div className="conu">
-    {detailsInfo.country}
+      <div className="details-info">
+        <div className="condition">
+          <h1>{detailsInfo.condition}</h1>
+          <img src={detailsInfo.img} alt="icon"></img>
+        </div>
+        <div className="details-container">
+          <p>wind speed: {detailsInfo.wind} mph</p>
+          <p>feels temp: {detailsInfo.current} °C</p>
+          <p>humidity: {detailsInfo.humidityinf} </p>
+        </div>
+      </div>
     </div>
+  )
+}
 
-  <div className="details-info">
-    <div className="condition">
-      <h1>{detailsInfo.condition}</h1>
-      <img src={detailsInfo.img} alt="icon"></img>
-    </div>
-    <div className="details-container">
-      <p>wind speed: {detailsInfo.wind} mph</p>
-      <p>feels temp: {detailsInfo.current} °C</p>
-      <p>humidity: {detailsInfo.humidityinf} </p>
-    </div>
-  </div>
-</div>
-)}
-
-export default Details
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,51 +1,62 @@
-import { useState } from "react"; 
+import { useState, useEffect } from "react"; 
 import { useNavigate } from "react-router-dom"; 
 
 export default function Search() {
 
-const [city, setCity] = useState("")
-const [cityInfo, setCityInfo] = useState({})
 let navigate = useNavigate();
+const APIkey = "5f120783051b409e82f14337220807"
+
+const [cityInfo, setCityInfo] = useState("")
+const [city, setCity] = useState ( () => {
+const savedItem = localStorage.getItem("city");
+const parsedItem = JSON.parse(savedItem);
+  return parsedItem || "";
+});
+
 
 const SearchApi = () => {
-  fetch(`http://api.weatherapi.com/v1/forecast.json?key=5f120783051b409e82f14337220807&q=${city}&days=1&aqi=no&alerts=no`)
+  fetch([`http://api.weatherapi.com/v1/forecast.json?key=${APIkey}&q=${city}&days=1&aqi=no&alerts=no`])
   .then(response => response.json())
   .then((data) => setCityInfo({
     name: data.location.name,
     country: data.location.country,
     current: data.current.temp_c,
-    
   }))
 };
+
+useEffect(() => {
+  localStorage.setItem("city", JSON.stringify(city))
+})
+
 return (
-  
-<div className="Search">
-<h2>Simple Weather App</h2>
-  <div className ="location-name">
-    <p>Location</p>
-  <div className ="search-container">
-    <input  
-      type ="text" 
-      placeholder="Enter a city"
-      value={city}
-      onChange={(event) => setCity(event.target.value)}
-      />
+    <div className="Search">
+      <h2>Simple Weather App</h2>
+        <div className ="location-name">
+          <p>Location</p>
+        <div className ="search-container">
+            <input  
+              type ="text"
+              name="city" 
+              placeholder="Enter a city"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+            />
 
-      <button onClick={SearchApi}>Search</button>
-      <button 
-      onClick={() => {
-        navigate ("/details");
-      }}>more details
-      </button>
-
-  </div>
-  <div className ="weather-container">
-    <div className ="weather">{cityInfo.current}°C</div>
-  </div>
-  <div className ="location-container">
-    <div className ="location">{cityInfo.country}{" "}{cityInfo.name}</div>
-  </div>
-</div>
-</div>
-
-)}
+            <button onClick={SearchApi}>Search</button>
+            <button 
+            onClick={() => {
+              navigate ("/details");
+            }}>more details
+            </button>
+        </div>  
+         
+        <div className ="weather-container">
+          <div className ="weather">{cityInfo.current}°C</div>
+        </div>
+        <div className ="location-container">
+          <div className ="location">{cityInfo.country}{" "}{cityInfo.name}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
