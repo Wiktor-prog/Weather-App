48b60505f555efb51f86e5ec8fc76b8e20e8aa88
Wiktor Ziejewski <wiktor.ziejewski>
2022-08-11 09:21:53 +0200

    save weather data in local storage, andadd new visual style.css

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react"; 
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaWater, FaWind, FaTemperatureHigh} from 'react-icons/fa';
 import "../Search/Search.css"
 import  "../Search/Details.css"
 
-
 export default function Search() {
-  
-const APIkey = "5f120783051b409e82f14337220807"
 
-const [cityInfo, setCityInfo] = useState ([])
-const [city, setCity] = useState ( () => {
+  const APIkey = "5f120783051b409e82f14337220807"
 
-  const saveCity = localStorage.getItem('city');
-  const parsedCity = JSON.parse(saveCity);
+  const [cityInfo, setCityInfo] = useState ( () => {
+      const saveInfo = localStorage.getItem("cityInfo");
+      const parsedInfo = JSON.parse(saveInfo);
+    
+    return parsedInfo || "";
+})
 
-  return parsedCity || "";
+        const [city, setCity] = useState ( () => {
+            const saveCity = localStorage.getItem("city");
+            const parsedCity = JSON.parse(saveCity);
+
+          return parsedCity || "";
 });
 
+
   const SearchApi = () => {
-    fetch([`http://api.weatherapi.com/v1/forecast.json?key=${APIkey}&q=${city}&days=1&aqi=no&alerts=no`])
+    fetch([`http://api.weatherapi.com/v1/forecast.json?key=
+    ${APIkey}&q=${city}&days=1&aqi=no&alerts=no`])
 
     .then(response => 
       response.json())
@@ -36,14 +42,20 @@ const [city, setCity] = useState ( () => {
             condition: data.current.condition.text,
 
             img: data.current.condition.icon
-
     }))
   };
 
-  useEffect(() => {
-    window.localStorage.setItem("city", JSON.stringify(city))
-  })
-  
+
+    useEffect(() => {
+      window.localStorage.setItem
+      ("city", JSON.stringify(city))
+    });
+
+        useEffect(() => {
+          window.localStorage.setItem
+          ("cityInfo", JSON.stringify(cityInfo))
+        });
+
 
   return (
       <div className="Search">
@@ -51,8 +63,8 @@ const [city, setCity] = useState ( () => {
         <h2>Simple Weather App</h2>
           <div className ="location-name">
             
-            <p>Location</p>
           <div className ="search-container">
+          <p>Location</p>
               <input  
                 type ="text"
                 name="city" 
@@ -76,20 +88,30 @@ const [city, setCity] = useState ( () => {
           </div>
 
           <div className="Details">
-            <div className="details-info">
               <div className="condition">
                   <h1>{cityInfo.condition}</h1>
                   <img src={cityInfo.img} alt="icon"></img>
               </div>
 
               <div className="details-container">
-                  <p>feels temp: {cityInfo.current} °C</p>
-                  <p>wind speed: {cityInfo.wind} mph</p>
-                  <p>humidity: {cityInfo.humidityinf} </p>
+                <div className="content"> 
+                <FaTemperatureHigh /> 
+                  <p>feels temp: {cityInfo.current}&deg;C</p>
+                </div>
+
+                <div className="content"> 
+                <FaWater /> 
+                  <p>humidity: {cityInfo.humidityinf}</p>
+                </div>
+
+                <div className="content"> 
+                <FaWind /> 
+                  <p>wind: {cityInfo.wind} mph</p>
+                </div>
               </div>
 
             </div>
           </div>
-      </div>
+    
     );
   }
